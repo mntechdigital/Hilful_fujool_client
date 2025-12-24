746ed3f75c8615e3870b8f8f53f3f472990411e3
reshad21 <miakala7@gmail.com>
2025-12-24 11:08:17 +0600

    feat: Update HeroSection and AboutUs components; replace mosque icon and optimize image handling for better performance

--- src/app/(common)/about-us/_components/AboutUs.tsx
+++ src/app/(common)/about-us/_components/AboutUs.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import img1 from "../../../../../public/icons/mosque.svg"
 import img2 from "../../../../../public/icons/about-us.png"
 import img3 from "../../../../../public/icons/about-us-1.png"
 import img4 from "../../../../../public/icons/about-us-2.png"
@@ -22,19 +21,21 @@ const AboutUs = async () => {
                     {/* Left column - 2 images, top larger than bottom */}
                     <div className="flex flex-col gap-3 sm:gap-4">
                         {/* Top left - larger image */}
-                        <div className="rounded-2xl overflow-hidden aspect-[3/4]">
-                            <img
+                        <div className="rounded-2xl overflow-hidden aspect-[3/4] relative">
+                            <Image
                                 src={aboutUsData.aboutUsImages[0].image}
                                 alt="about-1"
-                                className="object-cover w-full h-full"
+                                fill
+                                className="object-cover"
                             />
                         </div>
                         {/* Bottom left - smaller image */}
-                        <div className="rounded-2xl overflow-hidden aspect-[3/2]">
-                            <img
+                        <div className="rounded-2xl overflow-hidden aspect-[3/2] relative">
+                            <Image
                                 src={aboutUsData.aboutUsImages[1].image}
                                 alt="about-2"
-                                className="object-cover w-full h-full"
+                                fill
+                                className="object-cover"
                             />
                         </div>
                     </div>
@@ -42,19 +43,21 @@ const AboutUs = async () => {
                     {/* Right column - 2 images, top smaller than bottom */}
                     <div className="flex flex-col gap-3 sm:gap-4">
                         {/* Top right - smaller image */}
-                        <div className="rounded-2xl overflow-hidden aspect-[3/2]">
-                            <img
+                        <div className="rounded-2xl overflow-hidden aspect-[3/2] relative">
+                            <Image
                                 src={aboutUsData.aboutUsImages[2].image}
                                 alt="about-3"
-                                className="object-cover w-full h-full"
+                                fill
+                                className="object-cover"
                             />
                         </div>
                         {/* Bottom right - larger image (Kaaba) */}
-                        <div className="rounded-2xl overflow-hidden aspect-[3/4]">
-                            <img
+                        <div className="rounded-2xl overflow-hidden aspect-[3/4] relative">
+                            <Image
                                 src={aboutUsData.aboutUsImages[3].image}
                                 alt="about-4"
-                                className="object-cover w-full h-full"
+                                fill
+                                className="object-cover"
                             />
                         </div>
                     </div>
@@ -65,7 +68,7 @@ const AboutUs = async () => {
             <div className="flex flex-col gap-6">
                 {/* Section label */}
                 <div className="flex items-center gap-2 text-yellow-600 font-semibold text-lg">
-                    <Image src={img1} alt="icon" width={24} height={24} />
+                    <Image src={img2} alt="icon" width={24} height={24} />
                     <span>আমাদের সম্পর্কে</span>
                 </div>
                 
@@ -85,7 +88,7 @@ const AboutUs = async () => {
                     <div className="col-span-1 md:col-span-2 flex flex-col gap-4">
                         {/* Feature Card 1 */}
                         <div className="flex items-start gap-4 bg-[#F8F8F8] rounded-xl p-5 shadow-sm">
-                            <Image src={img1} alt="icon" width={40} height={40} />
+                            <Image src={img2} alt="icon" width={40} height={40} />
                             <div>
                                 <h3 className="font-bold text-lg text-[#184C43]">
                                     {aboutUsData.featureTitle1}
@@ -98,7 +101,7 @@ const AboutUs = async () => {
                         
                         {/* Feature Card 2 */}
                         <div className="flex items-start gap-4 bg-[#F8F8F8] rounded-xl p-5 shadow-sm">
-                            <Image src={img2} alt="icon" width={40} height={40} />
+                            <Image src={img3} alt="icon" width={40} height={40} />
                             <div>
                                 <h3 className="font-bold text-lg text-[#184C43]">
                                     {aboutUsData.featureTitle2}
@@ -112,9 +115,9 @@ const AboutUs = async () => {
 
                     {/* Goal Card - Assuming this should use featureTitle3 and featureShortDesc3 */}
                     <div className="md:col-span-1">
-                        <div className="flex flex-col gap-4 bg-[#F8F8F8] rounded-xl p-5 shadow-sm h-full justify-between">
+                        <div className="flex flex-col gap-4 bg-[#F8F8F8] rounded-xl p-3 shadow-sm h-full justify-between items-center text-center">
                             <span className="inline-flex items-center justify-center w-12 h-12 rounded-full border-2 border-yellow-600">
-                                <Image src={img3} alt="icon" width={40} height={40} />
+                                <Image src={img4} alt="icon" width={40} height={40} />
                             </span>
                             <div>
                                 <h3 className="font-bold text-lg text-[#184C43]">
@@ -129,7 +132,7 @@ const AboutUs = async () => {
                                 href="/services" 
                                 className="px-6 py-2 bg-yellow-600 text-white rounded-full font-semibold text-center hover:bg-yellow-700 transition"
                             >
-                                সব সেবা
+                                All Services
                             </Link>
                         </div>
                     </div>
