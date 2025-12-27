7420b60f2f0a2fcf26438b7e1207b0323bfb630c
reshad21 <miakala7@gmail.com>
2025-12-27 11:19:04 +0600

    feat: enhance UpdateProfileCRUD component with improved form handling and validation; integrate profile data fetching in UpdateProfilePage

--- src/app/(dashboard)/dashboard/update-profile/page.tsx
+++ src/app/(dashboard)/dashboard/update-profile/page.tsx
@@ -1,12 +1,30 @@
-import React from "react";
-import UpdateProfileCRUD from "./_components/UpdateProfileCRUD";
+import { loggedUser } from "@/services/auth";
 import { DashboardWrapper } from "../_components/DashboardWrapper";
+import UpdateProfileCRUD, { UpdateProfileFormData } from "./_components/UpdateProfileCRUD";
 
-const UpdateProfilePage = () => {
+const UpdateProfilePage = async () => {
+  const updateProfileRes = await loggedUser();
+  console.log("logged user infor==>", updateProfileRes);
+
+  // Provide a fallback/default value if updateProfileRes is null
+  const defaultProfileData: UpdateProfileFormData = {
+    id: "",
+    email: "",
+    fullName: "",
+    role: "",
+    status: "",
+    profilePhoto: null,
+    phoneNumber: "",
+    image: null,
+    password: "",
+    newPassword: "",
+    retypePassword: "",
+  };
+
   return (
     <DashboardWrapper>
       <h1 className="text-2xl font-semibold text-[#0f3d3e] mb-6">Update Profile</h1>
-      <UpdateProfileCRUD />
+      <UpdateProfileCRUD profileData={updateProfileRes ?? defaultProfileData} />
     </DashboardWrapper>
   );
 };
