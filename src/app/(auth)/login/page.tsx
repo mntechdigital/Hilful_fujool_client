863311ebd72fd13945a9ec66a2620fa8199bbb7e
reshad21 <miakala7@gmail.com>
2025-12-28 14:05:45 +0600

    Add footer icon image to the public directory

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,10 +3,8 @@ import LoginForm from "../_components/LoginForm";
 
 const Login = () => {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100 ">
-      <div className="max-w-lg shadow-md rounded-md bg-white pt-5">
-        <LoginForm/>
-      </div>
+    <div className=" bg-gray-100 ">
+      <LoginForm/>
     </div>
   );
 };
