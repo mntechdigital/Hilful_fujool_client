a5bcec4b7179cd2ebd0e6fb4ceb362222eef0339
reshad21 <miakala7@gmail.com>
2025-12-31 15:09:58 +0600

    fix: adjust padding in ServiceHeader component for better layout

--- src/app/(common)/services/_components/ServiceHeader.tsx
+++ src/app/(common)/services/_components/ServiceHeader.tsx
@@ -7,7 +7,7 @@ const ServiceHeader =  async () => {
   const dynamicTitle = serviceTitle?.data[0].serviceTitle;
 
   return (
-    <div className="flex flex-col items-center justify-center py-6">
+    <div className="flex flex-col items-center justify-center pt-6 pb-2">
       <div className="flex items-center gap-2 mb-2">
         <Image src={img2} alt="service icon" width={24} height={24} unoptimized/>
         <span className="text-xl text-[#0f3d3e] font-medium">আমাদের সেবাসমূহ</span>
